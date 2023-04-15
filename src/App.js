0e67c4bb3605bbac9f68649f96f16b9c16a868ff
Ravi Prakash Singh <ssingh.raviprakash@gmail.com>
2023-04-15 16:47:27 +0530

    centered and beautified

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import axios from 'axios';
 import Paper from '@mui/material/Paper';
-
-
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const App = () => {
   const [amount, setAmount] = useState(1000);
@@ -30,47 +33,65 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>Financial Calculator</h1>
-      <label>
-        Amount Invested:
-        <input type="number" value={amount} onChange={handleAmountChange} />
-      </label>
-      <br />
-      <label>
-        Time:
-        <input type="number" value={time} onChange={handleTimeChange} />
-      </label>
-      <br />
-      <button onClick={handleCalculate}>Calculate</button>
-      <br />
-      {financialInstruments.length > 0 ? (
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Instrument Name</TableCell>
-                <TableCell align="right">Rate</TableCell>
-                <TableCell align="right">Maturing Amount</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {financialInstruments.map((instrument, index) => (
-                <TableRow key={index}>
-                  <TableCell component="th" scope="row">
-                    {instrument['Instrument Name']}
-                  </TableCell>
-                  <TableCell align="right">{instrument['Rate']}</TableCell>
-                  <TableCell align="right">
-                    {instrument['Maturing Amount']}
-                  </TableCell>
+    <Container>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: 4,
+        }}
+      >
+        <Typography variant="h3" gutterBottom>
+          Financial Calculator
+        </Typography>
+        <TextField
+          label="Amount Invested"
+          value={amount}
+          onChange={handleAmountChange}
+          type="number"
+          variant="outlined"
+          margin="normal"
+          sx={{ width: '50%' }}
+        />
+        <TextField
+          label="Time"
+          value={time}
+          onChange={handleTimeChange}
+          type="number"
+          variant="outlined"
+          margin="normal"
+          sx={{ width: '50%' }}
+        />
+        <Button variant="contained" onClick={handleCalculate} sx={{ width: '50%', marginTop: 2 }}>
+          Calculate
+        </Button>
+        {financialInstruments.length > 0 ? (
+          <TableContainer component={Paper} sx={{ width: '50%', marginTop: 4 }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Instrument Name</TableCell>
+                  <TableCell align="right">Rate</TableCell>
+                  <TableCell align="right">Maturing Amount</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      ) : null}
-    </div>
+              </TableHead>
+              <TableBody>
+                {financialInstruments.map((instrument, index) => (
+                  <TableRow key={index}>
+                    <TableCell component="th" scope="row">
+                      {instrument['Instrument Name']}
+                    </TableCell>
+                    <TableCell align="right">{instrument['Rate']}</TableCell>
+                    <TableCell align="right">{instrument['Maturing Amount']}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : null}
+      </Box>
+    </Container>
   );
 };
 
